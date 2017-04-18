a436843011ed7fbe20b0443fcd125cffe3768f07
Ameen Demidem <ameen.demidem@gmail.com>
2017-04-18 14:56:06 -0400

    Add helper for page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def full_title(title)
+    sub_title = title || ''
+    full_title = 'ToyApp'
+    full_title = "#{sub_title} | #{full_title}" unless sub_title.empty?
+    return full_title
+  end
 end
