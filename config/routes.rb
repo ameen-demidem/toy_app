be89a7c63f99e4d50f0df5afde34b4ee9625d9ef
Ameen Demidem <ameen.demidem@gmail.com>
2017-04-18 13:05:06 -0400

    Add StaticPages controller skeleton

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get '/home', controller: :static_pages, action: :home
+  get '/help', controller: :static_pages, action: :help
+
   resources :microposts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
