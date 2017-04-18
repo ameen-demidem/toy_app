0a5d36b43f349cd6c25dbe7c06ec46035aaed302
Ameen Demidem <ameen.demidem@gmail.com>
2017-04-18 12:15:38 -0400

    Add validation to the models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :microposts
+  validates :name, presence: true
+  validates :email,
+    presence: true,
+    format: { with: /\A[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-_]+\.[a-zA-Z]+\z/ }
 end
