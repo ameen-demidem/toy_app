a54f3085288eaecfe1b31d52e5e8323bb2fadcfc
Ameen Demidem <ameen.demidem@gmail.com>
2017-04-19 08:27:10 -0400

    Make sure users's emails are unique and normalized to lower case when saved

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   has_many :microposts
   validates :name, presence: true
   validates :email,
     presence: true,
-    format: { with: /\A[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-_]+\.[a-zA-Z]+\z/ }
+    format: { with: /\A[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-_]+\.[a-zA-Z]+\z/ },
+    uniqueness: { case_sensitive: false }
 end
